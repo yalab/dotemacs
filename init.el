a5b5c0af26e2ba9764d569ce269a5e3e93a04609
yalab <rudeboyjet@gmail.com>
2022-10-14 09:40:27 +0900

    install nyan-mode

--- init.el
+++ init.el
@@ -56,7 +56,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(flycheck migemo)))
+ '(package-selected-packages '(nyan-mode flycheck migemo)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -81,3 +81,7 @@
 (migemo-init)
 
 (require 'flycheck)
+
+(require 'nyan-mode)
+(nyan-mode)
+(nyan-start-animation)
