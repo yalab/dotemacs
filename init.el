70df754c342cf0f5689714a33d3b418b36e368ee
yalab <rudeboyjet@gmail.com>
2022-11-11 18:30:58 +0900

    install rbs-mode

--- init.el
+++ init.el
@@ -57,7 +57,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(lsp-mode elm-mode typescript-mode counsel nyan-mode flycheck migemo)))
+   '(rbs-mode lsp-mode elm-mode typescript-mode counsel nyan-mode flycheck migemo)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
