7791c020e0a347f82483c848f0e03f54128c7838
yalab <rudeboyjet@gmail.com>
2022-11-22 09:28:51 +0900

    install yaml-mode

--- init.el
+++ init.el
@@ -57,7 +57,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(rbs-mode lsp-mode elm-mode typescript-mode counsel nyan-mode flycheck migemo)))
+   '(yaml-mode rbs-mode lsp-mode elm-mode typescript-mode counsel nyan-mode flycheck migemo)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
