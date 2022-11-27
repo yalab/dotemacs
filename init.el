878bcc51e6f918bfa89f0345d902d4e692286991
yalab <rudeboyjet@gmail.com>
2022-11-27 16:12:46 +0900

    Fix flycheck warning.

--- init.el
+++ init.el
@@ -1,3 +1,7 @@
+;;; init.el --- initialize yalab emacs   -*- lexical-binding: t; -*-
+;;; Commentary:
+;;; Code:
+
 (setq inhibit-startup-message t)
 (setq make-backup-files nil)
 (setq auto-save-default nil)
@@ -8,41 +12,41 @@
          (menu-bar-mode -1)))
 
 ; color
-(setq black  "black")
-(setq red    "#ff6666")
-(setq green  "#66ff66")
-(setq yellow "#ffd314")
-(setq blue   "#6699ff")
-(setq cyan   "cyan")
-(setq white  "white")
-(setq magenta "#9966ff")
-(set-face-foreground 'font-lock-comment-face red)
-(set-face-foreground 'font-lock-string-face  green)
-(set-face-foreground 'font-lock-keyword-face cyan)
-(set-face-bold-p     'font-lock-keyword-face t)
-(set-face-foreground 'font-lock-function-name-face blue)
-(set-face-bold-p     'font-lock-function-name-face t)
-(set-face-foreground 'font-lock-variable-name-face yellow)
-(set-face-foreground 'font-lock-type-face green)
-(set-face-foreground 'font-lock-builtin-face magenta)
-(set-face-foreground 'font-lock-constant-face magenta)
-(set-face-foreground 'font-lock-warning-face white)
-(set-face-bold-p 'font-lock-warning-face nil)
+(let (red green yellow blue cyan white magenta)
+  (setq red    "#ff6666")
+  (setq green  "#66ff66")
+  (setq yellow "#ffd314")
+  (setq blue   "#6699ff")
+  (setq cyan   "cyan")
+  (setq white  "white")
+  (setq magenta "#9966ff")
+  (set-face-foreground 'font-lock-comment-face red)
+  (set-face-foreground 'font-lock-string-face  green)
+  (set-face-foreground 'font-lock-keyword-face cyan)
+  (set-face-bold     'font-lock-keyword-face t)
+  (set-face-foreground 'font-lock-function-name-face blue)
+  (set-face-bold     'font-lock-function-name-face t)
+  (set-face-foreground 'font-lock-variable-name-face yellow)
+  (set-face-foreground 'font-lock-type-face green)
+  (set-face-foreground 'font-lock-builtin-face magenta)
+  (set-face-foreground 'font-lock-constant-face magenta)
+  (set-face-foreground 'font-lock-warning-face white)
+  (set-face-bold 'font-lock-warning-face nil))
 
-(setq font-family "Ricty")
-
-(if window-system
-  (progn (set-background-color "Black")
-         (set-foreground-color "LightGray")
-         (set-cursor-color "Gray")
-         (set-frame-parameter nil 'alpha 90)
-         (setq default-frame-alist (append (list '(width . 95)
-						 '(height . 30))
-					   default-frame-alist))
-         (set-face-attribute 'default nil :family font-family :height 140)
-         (set-fontset-font
-          nil 'japanese-jisx0208
-          (font-spec :family font-family))))
+(let (font-family)
+  (setq font-family "Ricty")
+  (if window-system
+      (progn (set-background-color "Black")
+             (set-foreground-color "LightGray")
+             (set-cursor-color "Gray")
+             (set-frame-parameter nil 'alpha 90)
+             (setq default-frame-alist (append (list '(width . 95)
+						     '(height . 30))
+					       default-frame-alist))
+             (set-face-attribute 'default nil :family font-family :height 140)
+             (set-fontset-font
+              nil 'japanese-jisx0208
+              (font-spec :family font-family)))))
 
 ; package
 (require 'package)
@@ -69,24 +73,26 @@
   (progn (add-to-list 'exec-path "/opt/homebrew/bin")
          (setq ns-command-modifier (quote meta))))
 
-(setq migemo-command "cmigemo")
-(setq migemo-options '("-q" "--emacs" "-i" "\g"))
-(cond ((string= system-type "darwin")
-       (setq migemo-dictionary "/opt/homebrew/Cellar/cmigemo/20110227/share/migemo/utf-8/migemo-dict"))
-      (t
-       (setq migemo-dictionary "/usr/share/cmigemo/utf-8/migemo-dict")))
-(setq migemo-coding-system 'utf-8-unix)
-(setq migemo-user-dictionary nil)
-(setq migemo-regex-dictionary nil)
-(load-library "migemo")
-(migemo-init)
+(require 'migemo)
+(let (migemo-command migemo-options migemo-dictionary migemo-coding-system migemo-user-dictionary migemo-regex-dictionary)
+  (setq migemo-command "cmigemo")
+  (setq migemo-options '("-q" "--emacs" "-i" "\g"))
+  (cond ((string= system-type "darwin")
+         (setq migemo-dictionary "/opt/homebrew/Cellar/cmigemo/20110227/share/migemo/utf-8/migemo-dict"))
+        (t
+         (setq migemo-dictionary "/usr/share/cmigemo/utf-8/migemo-dict")))
+  (setq migemo-coding-system 'utf-8-unix)
+  (setq migemo-user-dictionary nil)
+  (setq migemo-regex-dictionary nil)
+  (load-library "migemo")
+  (migemo-init))
 
 (require 'flycheck)
 (add-hook 'ruby-mode-hook
-          '(lambda ()
+          #'(lambda ()
              (setq flycheck-checker 'ruby-rubocop)))
 (add-hook 'typescript-mode-hook
-          '(lambda ()
+          #'(lambda ()
              (setq flycheck-checker 'javascript-eslint)))
 (flycheck-mode 1)
 
@@ -116,3 +122,5 @@
   (lambda () (interactive) (indent-rigidly (region-beginning) (region-end)  2)))
 (global-set-key "\C-cu"
   (lambda () (interactive) (indent-rigidly (region-beginning) (region-end) -2)))
+
+;;; init.el ends here
